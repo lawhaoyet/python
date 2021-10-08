f199445ce6762c06a3168dd73c0b8c005e889f26
lawhaoyet <87408313+lawhaoyet@users.noreply.github.com>
2021-10-08 09:26:17 +0800

    Create main.py

---
+++ main.py
@@ -0,0 +1,283 @@
+import discord
+from discord.ext import commands, tasks
+import asyncio
+from itertools import cycle
+import os
+import json
+import random
+
+client = commands.Bot(command_prefix='cli ')
+
+client.remove_command("help")
+
+
+@client.event
+async def on_ready():
+    await client.change_presence(status=discord.Status.online,
+                                 activity=discord.Game('cli help'))
+    print('connecting to we server...')
+    print('downloading web server...')
+    print('breacking to we server...')
+    print('connected to discord')
+    print('connected to web server')
+    print('hacking discord server system...')
+    print('hacked!')
+    print('launching discord...')
+    print('launched!')
+    print('done!')
+
+
+mainshop = [{
+    "name": "Watch",
+    "price": 100,
+    "description": "Time"
+}, {
+    "name": "Laptop",
+    "price": 1000,
+    "description": "Work"
+}, {
+    "name": "PC",
+    "price": 10000,
+    "description": "Gaming"
+}, {
+    "name": "Ferrari",
+    "price": 99999,
+    "description": "Sports Car"
+}]
+
+
+@client.command(aliases=['bal'])
+async def balance(ctx):
+    await open_account(ctx.author)
+    user = ctx.author
+
+    users = await get_bank_data()
+
+    wallet_amt = users[str(user.id)]["wallet"]
+    bank_amt = users[str(user.id)]["bank"]
+
+    em = discord.Embed(title=f'{ctx.author.name} Balance',
+                       color=discord.Color.red())
+    em.add_field(name="Wallet Balance", value=wallet_amt)
+    em.add_field(name='Bank Balance', value=bank_amt)
+    await ctx.send(embed=em)
+
+
+async def open_account(user):
+
+    users = await get_bank_data()
+
+    if str(user.id) in users:
+        return False
+    else:
+        users[str(user.id)] = {}
+        users[str(user.id)]["wallet"] = 0
+        users[str(user.id)]["bank"] = 0
+
+    with open('mainbank.json', 'w') as f:
+        json.dump(users, f)
+
+    return True
+
+
+async def get_bank_data():
+    with open('mainbank.json', 'r') as f:
+        users = json.load(f)
+
+    return users
+
+
+async def update_bank(user, change=0, mode='wallet'):
+    users = await get_bank_data()
+
+    users[str(user.id)][mode] += change
+
+    with open('mainbank.json', 'w') as f:
+        json.dump(users, f)
+    bal = users[str(user.id)]['wallet'], users[str(user.id)]['bank']
+    return bal
+
+
+#other
+
+
+@client.command()
+async def shop(ctx):
+    em = discord.Embed(title="Shop")
+
+    for item in mainshop:
+        name = item["name"]
+        price = item["price"]
+        desc = item["description"]
+        em.add_field(name=name, value=f"${price} | {desc}")
+
+    await ctx.send(embed=em)
+
+
+@client.command()
+async def buy(ctx, item, amount=1):
+    await open_account(ctx.author)
+
+    res = await buy_this(ctx.author, item, amount)
+
+    if not res[0]:
+        if res[1] == 1:
+            await ctx.send("That Object isn't there!")
+            return
+        if res[1] == 2:
+            await ctx.send(
+                f"You don't have enough money in your wallet to buy {amount} {item}"
+            )
+            return
+
+    await ctx.send(f"You just bought {amount} {item}")
+
+
+@client.command()
+async def inv(ctx):
+    await open_account(ctx.author)
+    user = ctx.author
+    users = await get_bank_data()
+
+    try:
+        bag = users[str(user.id)]["bag"]
+    except:
+        bag = []
+
+    em = discord.Embed(title="Bag")
+    for item in bag:
+        name = item["item"]
+        amount = item["amount"]
+
+        em.add_field(name=name, value=amount)
+
+    await ctx.send(embed=em)
+
+
+async def buy_this(user, item_name, amount):
+    item_name = item_name.lower()
+    name_ = None
+    for item in mainshop:
+        name = item["name"].lower()
+        if name == item_name:
+            name_ = name
+            price = item["price"]
+            break
+
+    if name_ == None:
+        return [False, 1]
+
+    cost = price * amount
+
+    users = await get_bank_data()
+
+    bal = await update_bank(user)
+
+    if bal[0] < cost:
+        return [False, 2]
+
+    try:
+        index = 0
+        t = None
+        for thing in users[str(user.id)]["bag"]:
+            n = thing["item"]
+            if n == item_name:
+                old_amt = thing["amount"]
+                new_amt = old_amt + amount
+                users[str(user.id)]["bag"][index]["amount"] = new_amt
+                t = 1
+                break
+            index += 1
+        if t == None:
+            obj = {"item": item_name, "amount": amount}
+            users[str(user.id)]["bag"].append(obj)
+    except:
+        obj = {"item": item_name, "amount": amount}
+        users[str(user.id)]["bag"] = [obj]
+
+    with open("mainbank.json", "w") as f:
+        json.dump(users, f)
+
+    await update_bank(user, cost * -1, "wallet")
+
+    return [True, "Worked"]
+
+
+@client.command(aliases=['wd'])
+async def wit(ctx, amount=None):
+    await open_account(ctx.author)
+    if amount == None:
+        await ctx.send("Please enter the amount")
+        return
+
+    bal = await update_bank(ctx.author)
+
+    amount = int(amount)
+
+    if amount > bal[1]:
+        await ctx.send('You do not have sufficient balance')
+        return
+    if amount < 0:
+        await ctx.send('Amount must be positive!')
+        return
+
+    await update_bank(ctx.author, amount)
+    await update_bank(ctx.author, -1 * amount, 'bank')
+    await ctx.send(f'{ctx.author.mention} You withdrew {amount} coins')
+
+
+@client.command(aliases=['dp'])
+async def dep(ctx, amount=None):
+    await open_account(ctx.author)
+    if amount == None:
+        await ctx.send("Please enter the amount")
+        return
+
+    bal = await update_bank(ctx.author)
+
+    amount = int(amount)
+
+    if amount > bal[0]:
+        await ctx.send('You do not have sufficient balance')
+        return
+    if amount < 0:
+        await ctx.send('Amount must be positive!')
+        return
+
+    await update_bank(ctx.author, -1 * amount)
+    await update_bank(ctx.author, amount, 'bank')
+    await ctx.send(f'{ctx.author.mention} You deposited {amount} coins')
+
+
+@client.command(aliases=['sm'])
+async def give(ctx, member: discord.Member, amount=None):
+    await open_account(ctx.author)
+    await open_account(member)
+    if amount == None:
+        await ctx.send("Please enter the amount")
+        return
+
+    bal = await update_bank(ctx.author)
+    if amount == 'all':
+        amount = bal[0]
+
+    amount = int(amount)
+
+    if amount > bal[0]:
+        await ctx.send('You do not have sufficient balance')
+        return
+    if amount < 0:
+        await ctx.send('Amount must be positive!')
+        return
+
+    await update_bank(ctx.author, -1 * amount, 'bank')
+    await update_bank(member, amount, 'bank')
+    await ctx.send(f'{ctx.author.mention} You gave {member} {amount} coins')
+
+
+@client.command()
+async def hi(ctx):
+    await ctx.send("hi")
+
+
+client.run(os.getenv('TOKEN'))
